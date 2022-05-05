f72d83e46f0e874c4f9373c88e2cc6d924c269d7
James Ashbee <jamesashbee@hotmail.com>
2022-05-05 08:44:15 +0100

    V3 with Display Fix 5thMay22

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const btns = document.querySelectorAll(".JSCalculator__numberBtn");
 
 const decimalBtnPress = document.querySelector("#dec");
 
+const displayStoredNumber = document.querySelector(".stored-number");
+const displayStoredFNLastPressed = document.querySelector(
+  ".fn-button-last-pressed"
+);
+const displaySumSyntax = document.querySelector(".sum-syntax");
+
 let dispalyCurrentNumber = "";
 let displayNewNumber = "";
 let storedNumber = 0;
@@ -17,6 +23,7 @@ let sumResults = 0;
 let lastFNButtonPressed = "";
 let decimalPressed = false;
 let textDecimalNumber = "";
+let storedNumberPreEquals = 0;
 
 // Display Numbers on Screen when typed in
 btns.forEach((btn) => {
@@ -57,6 +64,7 @@ const resetAll = () => {
   lastFNButtonPressed = "";
   decimalPressed = false;
   textDecimalNumber = "";
+  storedNumberPreEquals = 0;
 };
 
 // Sum Function - called on +/-/x/divide or on equals
@@ -68,34 +76,35 @@ const sumTheNumbers = () => {
 
     switch (lastFNButtonPressed) {
       case "+":
-        sumResults =
-          parseFloat(storedNumber) + parseFloat(calculatorOutput.innerHTML);
+        storedNumberPreEquals = calculatorOutput.innerHTML;
+        sumResults = parseFloat(storedNumber) + parseFloat(calculatorOutput.innerHTML);
         break;
       case "-":
-        sumResults =
-          parseFloat(storedNumber) - parseFloat(calculatorOutput.innerHTML);
+        storedNumberPreEquals = calculatorOutput.innerHTML;
+        sumResults=parseFloat(storedNumber) - parseFloat(calculatorOutput.innerHTML);
         break;
       case "X":
-        sumResults =
-          parseFloat(storedNumber) * parseFloat(calculatorOutput.innerHTML);
+        storedNumberPreEquals = calculatorOutput.innerHTML;
+        sumResults = parseFloat(storedNumber) * parseFloat(calculatorOutput.innerHTML);
         break;
       case "/":
-        sumResults =
-          parseFloat(storedNumber) / parseFloat(calculatorOutput.innerHTML);
+        storedNumberPreEquals = calculatorOutput.innerHTML;
+        sumResults =parseFloat(storedNumber) / parseFloat(calculatorOutput.innerHTML);
         break;
     }
 
     // round up if more than 8 chars long like calculator
     let sumResLen = String(sumResults).length;
-    
-    if(sumResLen >8){
-      calculatorOutput.innerHTML = parseFloat(String(sumResults).substring(0, 8));
+
+    if (sumResLen > 8) {
+      calculatorOutput.innerHTML = parseFloat(
+        String(sumResults).substring(0, 8)
+      );
+      displaySumSyntax.innerHTML = `<p>The Calculation Was:<span><br>${storedNumber}<br>${lastFNButtonPressed}<br>${storedNumberPreEquals}</span></p>`;
+    } else {
+      calculatorOutput.innerHTML = sumResults;
+      displaySumSyntax.innerHTML = `<p>The Calculation Was:<span><br>${storedNumber}<br>${lastFNButtonPressed}<br>${storedNumberPreEquals}</span></p>`;
     }
-
-    else{
-    calculatorOutput.innerHTML = sumResults;
-  }
-
 
     // reset for next loop of fn
     resetAll();
@@ -112,6 +121,9 @@ const sumTheNumbers = () => {
 // reset when ac pressed
 clearBtn.addEventListener("click", () => {
   calculatorOutput.innerHTML = 0;
+  displayStoredFNLastPressed.innerHTML = ``;
+  displayStoredNumber.innerHTML = ``;
+  displaySumSyntax.innerHTML = ``;
 });
 
 clearBtn.addEventListener("click", resetAll);
@@ -126,6 +138,10 @@ fnBtns.forEach((fnBtn) => {
     storedNumber = calculatorOutput.innerHTML;
     calculatorOutput.innerHTML = 0;
 
+    // added for display last fn pressed
+    displayStoredFNLastPressed.innerHTML = `<p>Last function pressed was:<br><span>${lastFNButtonPressed}</span></p>`;
+    displayStoredNumber.innerHTML = `<p>Stored Number is:<br><span>${storedNumber}</span></p>`;
+
     //sumTheNumbers();
   });
 });
@@ -136,6 +152,10 @@ equalsBtn.addEventListener("click", sumTheNumbers);
 // fixed in v2
 // 2)If over say 10 chars long the result, round to 10 and align left
 
-/// To fix in v3
-// 1) if second fn gets pressed before equals
+// fixed in v3
 // 3)Display on page last number and last fn pressed
+// 4)Display on iphone should only be circa 90% high as address bar takes up bottom of screen
+
+/// To fix in v4
+// 1) if second fn gets pressed before equals
+
